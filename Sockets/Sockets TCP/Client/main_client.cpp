cafa0cf35ca3850fbf63956f4b2393f1e15707ac
GuillemD <gdrdvilla@gmail.com>
2019-10-12 20:33:57 +0200

    tcp socket client server implementation

--- Sockets/Sockets TCP/Client/main_client.cpp
+++ Sockets/Sockets TCP/Client/main_client.cpp
@@ -30,12 +30,35 @@ void printWSErrorAndExit(const char *msg)
 void client(const char *serverAddrStr, int port)
 {
 	// TODO-1: Winsock init
-
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NO_ERROR) {
+		printWSErrorAndExit("WSAStartup");
+	}
+	std::cout << "WSAStartup done" << std::endl;
 	// TODO-2: Create socket (IPv4, stream, TCP)
+	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+	if (s == INVALID_SOCKET) {
+		printWSErrorAndExit("socket");
+	}
+	std::cout << "socket done" << std::endl;
+
+	std::string pingString("Ping");
 
 	// TODO-3: Create an address object with the server address
-
+	struct sockaddr_in serverAddr;
+	const int serverAddrLen = sizeof(serverAddr);
+	serverAddr.sin_family = AF_INET; // IPv4
+	inet_pton(AF_INET, serverAddrStr, &serverAddr.sin_addr);
+	serverAddr.sin_port = htons(port); // Port
 	// TODO-4: Connect to server
+	int connectRes = connect(s, (const sockaddr*)&serverAddr, serverAddrLen);
+	if (connectRes == SOCKET_ERROR) {
+		printWSErrorAndExit("connect");
+	}
+	std::cout << "connect done" << std::endl;
+
+	const int inBufferLen = 1300;
+	char inBuffer[inBufferLen];
 
 	for (int i = 0; i < 5; ++i)
 	{
@@ -44,11 +67,42 @@ void client(const char *serverAddrStr, int port)
 		// - Receive 'pong' packet from the server
 		// - Control errors in both cases
 		// - Control graceful disconnection from the server (recv receiving 0 bytes)
+		
+		int bytes = send(s, pingString.c_str(), (int)pingString.size() + 1, 0);
+		if (bytes > 0)
+		{
+			std::cout << "Sent: " << pingString.c_str() << std::endl;
+
+			std::cout << "Waiting for server data... " << std::flush;
+
+			bytes = recv(s, inBuffer, inBufferLen, 0);
+			if (bytes == 0) {
+				std::cout << "connection closed by server" << std::endl;
+				break;
+			}
+			else if (bytes == SOCKET_ERROR) {
+				printWSErrorAndExit("recv");
+			}
+
+			std::cout << "Received: " << inBuffer << std::endl;
+
+			Sleep(1000);
+		}
+		else
+		{
+			int err = WSAGetLastError();
+			if (err == WSAECONNRESET) {
+				std::cout << "connection closed by the server" << std::endl;
+				break;
+			}
+			printWSErrorAndExit("send");
+		}
 	}
 
 	// TODO-6: Close socket
-
+	closesocket(s);
 	// TODO-7: Winsock shutdown
+	WSACleanup();
 }
 
 int main(int argc, char **argv)
--- Sockets/Sockets TCP/Server/main_server.cpp
+++ Sockets/Sockets TCP/Server/main_server.cpp
@@ -28,32 +28,94 @@ void printWSErrorAndExit(const char *msg)
 void server(int port)
 {
 	// TODO-1: Winsock init
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NO_ERROR) {
+		printWSErrorAndExit("WSAStartup");
+	}
+	std::cout << "WSAStartup done" << std::endl;
 
 	// TODO-2: Create socket (IPv4, stream, TCP)
-
+	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+	if (s == INVALID_SOCKET) {
+		printWSErrorAndExit("socket");
+	}
+	std::cout << "socket done" << std::endl;
 	// TODO-3: Configure socket for address reuse
-
+	int enable = 1;
+	int result = setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const char*)&enable, sizeof(int));
+	if (result == SOCKET_ERROR) {
+		printWSErrorAndExit("setsockopt");
+	}
+	std::cout << "setsockopt SO_REUSEADDR done" << std::endl;
 	// TODO-4: Create an address object with any local address
-
+	struct sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET; // IPv4
+	serverAddr.sin_addr.S_un.S_addr = INADDR_ANY; // Any address
+	serverAddr.sin_port = htons(port); // Port
 	// TODO-5: Bind socket to the local address
-
+	int bindRes = bind(s, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+	if (bindRes == SOCKET_ERROR) {
+		printWSErrorAndExit("bind");
+	}
+	std::cout << "bind done on port " << port << std::endl;
 	// TODO-6: Make the socket enter into listen mode
-
+	int listenRes = listen(s, 1);
+	if (listenRes == SOCKET_ERROR) {
+		printWSErrorAndExit("listen");
+	}
+	std::cout << "listen done" << std::endl;
 	// TODO-7: Accept a new incoming connection from a remote host
 	// Note that once a new connection is accepted, we will have
 	// a new socket directly connected to the remote host.
+	struct sockaddr clientAddr;
+	int clientAddrLen = sizeof(clientAddr);
+	SOCKET clientSocket = accept(s, (struct sockaddr*)&clientAddr, &clientAddrLen);
+	if (clientSocket == INVALID_SOCKET) {
+		printWSErrorAndExit("accept");
+	}
+	std::cout << "accept done" << std::endl;
 
+	std::string pongString("Pong");
+
+	const int inBufferLen = 1300;
+	char inBuffer[inBufferLen];
 	while (true)
 	{
 		// TODO-8:
 		// - Wait a 'ping' packet from the client
 		// - Send a 'pong' packet to the client
 		// - Control errors in both cases
+		std::cout << "Waiting for client data... " << std::flush;
+
+		int bytes = recv(clientSocket, inBuffer, inBufferLen, 0);
+		if (bytes > 0)
+		{
+			std::cout << "Received: " << inBuffer << std::endl;
+
+			Sleep(1000);
+
+			bytes = send(clientSocket, pongString.c_str(), (int)pongString.size() + 1, 0);
+			if (bytes == SOCKET_ERROR) {
+				printWSErrorAndExit("send");
+			}
+
+			std::cout << "Sent: '" << pongString.c_str() << "' sent" << std::endl;
+		}
+		else if (bytes == 0)
+		{
+			std::cout << "connection closed by client" << std::endl;
+			break;
+		}
+		else
+		{
+			printWSErrorAndExit("recv");
+		}
 	}
 
 	// TODO-9: Close socket
-
+	closesocket(s);
 	// TODO-10: Winsock shutdown
+	WSACleanup();
 }
 
 int main(int argc, char **argv)
